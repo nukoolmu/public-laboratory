83d49318f5ddb893bc39c3d63f516416eae15670
Nukool M <nukoolmuangsoon@gmail.com>
2022-12-12 12:08:33 +0000

    update main function

---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# SET PORTABLE PATH
+SCRIPT_PATH=$(readlink -f ${0})
+SCRIPT_NAME=$(basename ${0})
+SCRIPT_HOME=$(echo "${SCRIPT_PATH}"|sed "s|/${SCRIPT_NAME}||g")
+PROCESS_ID="$$"
+
+CONF_PATH=${SCRIPT_HOME}/etc
+TMP_PATH=${SCRIPT_HOME}/tmp
+
+# Include all functions
+
+for ifunc in $(find ${SCRIPT_HOME}/bin/  -type f) ;do
+    . $ifunc
+done
+
+# program start here
+
+
+banner
+echo 'Json Response Compare via Curl...'
+echo ''
+if [ -z ${1} ];then
+    filter "$CONF_PATH/default.csv"
+else
+
+    filter "$CONF_PATH/${1}.csv"
+fi
+
+echo ''
+echo 'Done!'
