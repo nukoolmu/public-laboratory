9ea07a42c37e448ce1c27282f3bb7a6be9ce6b03
Nukool M <nukoolmuangsoon@gmail.com>
2024-03-06 10:37:30 +0000

    forward

--- test_script.sh
+++ test_script.sh
@@ -1,11 +1,12 @@
 
 echo "Route Traffic via F5 (Whitelist IP)"
 # F5 IPs is 172.16.238.14 and proxy_pass to 172.16.238.15 (Forwarder) 
-# Expect result remote_addr = 172.16.238.14, x-forwarded-for = custom_clientIP 
+# Expect result remote_addr = 172.16.238.14, x-forwarded-for = 172.16.238.10 (3BB_Client1)
 docker exec -it public-laboratory_3bb_client1_1 curl -iG 172.16.238.14/ 
+# Expect result remote_addr = 172.16.238.14, x-forwarded-for = 172.16.238.11 (3BB_Client2)
 docker exec -it public-laboratory_3bb_client2_1 curl -iG 172.16.238.14/ 
 echo  '-----'
 echo "Route Traffic via AIS (Non-Whitelist IP)"
-# curl -iG 172.16.238.15 -H 'Host: sit.forwarder.lab.com' -H 'X-Forwarded-For: 1.2.3.4' #In Case custom X-Forwarded-For
 # Direct to Forwarder
-curl -iG 172.16.238.15 -H 'Host: sit.forwarder.lab.com' 
+# Expect result remove remote_addr, x-forwarded-for = 1.2.3.4 (That assumption is client IP)
+curl -iG 172.16.238.15 -H 'Host: sit.forwarder.lab.com' -H 'X-Forwarded-For: 1.2.3.4'
